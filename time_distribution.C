fe965e8cc4633551210a81f3aedcc9d7e804ecd0
Viola DiSilvestre <disilves@neutrino-01.novalocal>
2024-02-19 16:48:59 +0100

    new commit

--- time_distribution.C
+++ time_distribution.C
@@ -34,7 +34,7 @@ int main() {
     std::mt19937 g(rd());
     std::shuffle(time.begin(), time.end(), g);
     
-    TH1F *histogram = new TH1F("histogram", "time distribution", 100, 0, 70);
+    TH1F *histogram = new TH1F("histogram", "time distribution", 100, 55, 200);
 histogram->SetFillColor(kBlue); // Imposta il colore di riempimento dell'istogramma a rosso
 histogram->SetXTitle("time [ns]"); // Imposta l'etichetta dell'asse x
 histogram->SetYTitle("Counts"); // Imposta l'etichetta dell'asse y
