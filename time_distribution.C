87c995c9b4c140c6273f7dcc8fb87cb16850cde6
Viola DiSilvestre <disilves@neutrino-01.novalocal>
2024-02-20 19:28:42 +0100

    new commit

--- time_distribution.C
+++ time_distribution.C
@@ -39,12 +39,11 @@ int main() {
     std::shuffle(time.begin(), time.end(), g);
 
     // Creazione dell'istogramma di somma
-    //TH1F* sumHistogram = new TH1F("sumHistogram", "Sum of Normalized Histograms", 400, 58, 100);
-    TH1F* histogram = new TH1F("sumHistogram", "Sum of Normalized Histograms", 400, 58, 100);
+    TH1F* sumHistogram = new TH1F("sumHistogram", "Sum of Normalized Histograms", 400, 58, 100);
 
-    histogram->SetFillColor(kBlue);
-    histogram->SetXTitle("time [ns]");
-    histogram->SetYTitle("Voltage [V]");
+    sumHistogram->SetFillColor(kBlue);
+    sumHistogram->SetXTitle("time [ns]");
+    sumHistogram->SetYTitle("Voltage [V]");
 
     // Seleziona i primi n eventi dal vettore time (o meno se il vettore ha meno di n elementi)
     int numEventsToSelect = std::min(500, static_cast<int>(time.size()));
@@ -54,28 +53,30 @@ int main() {
 
     for (int i = 0; i < numEventsToSelect; ++i) {
         double mean = time[i];
-        //TH1F* histogram = new TH1F("histogram", "Normalized Histogram", 400, 58, 100);
+        TH1F* histogram = new TH1F("histogram", "Normalized Histogram", 400, 58, 100);
 
         // Riempimento e normalizzazione dell'istogramma corrente
         for (int j = 0; j < 1000; ++j) {
             histogram->Fill(generateGaussian(mean, sigma, g));
         }
-        /*double integral = histogram->Integral();
+        double integral = histogram->Integral();
         histogram->Scale(1.0 / integral);
 
-        // Riempimento dell'istogramma di somma con l'istogramma normalizzato
-        for (int bin = 1; bin <= histogram->GetNbinsX(); ++bin) {
-            double binContent = histogram->GetBinContent(bin);
-            double sumBinContent = sumHistogram->GetBinContent(bin);
-            sumHistogram->SetBinContent(bin, sumBinContent + binContent);
-        }
-
-        delete histogram; */ // Liberazione della memoria dall'istogramma corrente
+	// Controllo se l'istogramma di somma ha già dei dati
+	if (sumHistogram->Integral() == 0) {
+	  // Se è vuoto, copia l'istogramma corrente direttamente in sumHistogram
+	  sumHistogram = (TH1F*)histogram->Clone();
+	} else {
+	  // Altrimenti, aggiungi i contenuti dell'istogramma corrente a sumHistogram
+	  sumHistogram->Add(histogram);
+	}
+	
+        delete histogram;  // Liberazione della memoria dall'istogramma corrente
     }
 
     // Creazione del canvas e disegno dell'istogramma
     TCanvas *canvas = new TCanvas("canvas", "canvas", 800, 600);
-    histogram->Draw("hist");
+    sumHistogram->Draw("hist");
 
     // Salva l'istogramma su un file o visualizzalo a schermo
     canvas->SaveAs("time_distribution_all.png");
