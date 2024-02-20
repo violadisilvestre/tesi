97de48fab0991faad095f2b53157e6bdaec71b8b
Viola DiSilvestre <disilves@neutrino-01.novalocal>
2024-02-20 11:18:22 +0100

    new commit

--- time_distribution.C
+++ time_distribution.C
@@ -28,17 +28,6 @@ int main() {
     double value = std::stod(line);
     time.push_back(value);
   }
-  auto min_time = *std::min_element(time.begin(), time.end());
-  double threshold = min_time + 30.0;
-  
-  for (int i = 0; i < time.size(); ++i) {
-    if (time[i] > threshold) {
-      time.erase(time.begin() + i); // Cancella l'elemento dal vector
-      --i; // Decrementa l'indice per compensare la rimozione dell'elemento
-    }
-}
-
-  
   // Mescola il vettore time in modo casuale
   std::random_device rd;
   std::mt19937 g(rd());
@@ -46,13 +35,10 @@ int main() {
   
   TH1F *histogram = new TH1F("histogram", "time distribution", 400, 55, 100);
   histogram->SetFillColor(kBlue); // Imposta il colore di riempimento dell'istogramma a rosso
-  for (int i = 0; i < time.size(); ++i) {
-    histogram->Fill(time[i]);
-  }
   histogram->SetXTitle("time [ns]"); // Imposta l'etichetta dell'asse x
   histogram->SetYTitle("Counts"); // Imposta l'etichetta dell'asse y
-  // Seleziona i primi n eventi dal vettore time (o meno se il vettore ha meno di n elementi)
-  /*    int numEventsToSelect = std::min(500, static_cast<int>(time.size()));
+//Seleziona i primi n eventi dal vettore time (o meno se il vettore ha meno di n elementi)
+     int numEventsToSelect = std::min(500, static_cast<int>(time.size()));
 	
   // Creazione di gaussiane solo per gli eventi selezionati in modo casuale
   double sigma =0.35;
@@ -61,7 +47,7 @@ int main() {
   for (int j = 0; j < 1000; ++j) {
   histogram->Fill(generateGaussian(mean, sigma, g));
   }
-  }*/
+  }
   
   // Creazione del canvas e disegno dell'istogramma
   TCanvas *canvas = new TCanvas("canvas", "canvas", 800, 600);
