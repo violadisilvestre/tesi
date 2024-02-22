d890ccefd77e8d5788980f7f9f6898bc142825ff
Viola DiSilvestre <disilves@neutrino-01.novalocal>
2024-02-22 11:27:36 +0100

    new commit

--- time_distribution.C
+++ time_distribution.C
@@ -16,7 +16,7 @@ double generateGaussian(double mean, double sigma, std::mt19937 &gen) {
 
 int main() {
     // Apertura del file
-    std::ifstream file("dati.txt");
+    std::ifstream file("dati2.txt");
     if (!file.is_open()) {
         std::cerr << "Impossibile aprire il file!" << std::endl;
         return 1;
@@ -39,17 +39,17 @@ int main() {
     std::shuffle(time.begin(), time.end(), g);
 
     // Creazione dell'istogramma di somma
-    TH1F* sumHistogram = new TH1F("sumHistogram", "Sum of Normalized Histograms", 400, 58, 100);
-     TH1F* sumcutHistogram = new TH1F("sumHistogram", "Sum of Normalized Histograms", 400, 58, 73);
+    TH1F* sumHistogram = new TH1F("sumHistogram", "Sum of Normalized Histograms", 400, 50, 100);
+    //  TH1F* sumcutHistogram = new TH1F("sumHistogram", "Sum of Normalized Histograms", 400, 58, 73);
 
 
     sumHistogram->SetFillColor(kBlue);
     sumHistogram->SetXTitle("time [ns]");
     sumHistogram->SetYTitle("Voltage [V]");
 
-    sumcutHistogram->SetFillColor(kRed);
-    sumcutHistogram->SetXTitle("time [ns]");
-    sumcutHistogram->SetYTitle("Voltage [V]");
+    // sumcutHistogram->SetFillColor(kRed);
+    //sumcutHistogram->SetXTitle("time [ns]");
+    //sumcutHistogram->SetYTitle("Voltage [V]");
 
     // Seleziona i primi n eventi dal vettore time (o meno se il vettore ha meno di n elementi)
     int numEventsToSelect = std::min(500, static_cast<int>(time.size()));
@@ -59,26 +59,26 @@ int main() {
 
     for (int i = 0; i < numEventsToSelect; ++i) {
       double mean = time[i];
-      TH1F* histogram = new TH1F("histogram", "Normalized Histogram", 400, 58, 100);
-      TH1F* cuthistogram = new TH1F("histogram", "Normalized Histogram", 400, 58, 73);
+      TH1F* histogram = new TH1F("histogram", "Normalized Histogram", 400, 50, 100);
+      // TH1F* cuthistogram = new TH1F("histogram", "Normalized Histogram", 400, 58, 73);
       
       // Riempimento e normalizzazione dell'istogramma corrente
       for (int j = 0; j < 1000; ++j) {
 	histogram->Fill(generateGaussian(mean, sigma, g));
-	cuthistogram->Fill(generateGaussian(mean, sigma, g));
+	//	cuthistogram->Fill(generateGaussian(mean, sigma, g));
 			   
       }
 	  
 	  
       histogram->Scale(1.0 /1000);
-      cuthistogram->Scale(1.0/1000);
+      // cuthistogram->Scale(1.0/1000);
       
       // Aggiungi i contenuti dell'istogramma corrente a sumHistogram
       sumHistogram->Add(histogram);
-      sumcutHistogram->Add(cuthistogram);
+      //sumcutHistogram->Add(cuthistogram);
 
         delete histogram; 
-	delete cuthistogram;// Liberazione della memoria dall'istogramma corrente
+	//delete cuthistogram;// Liberazione della memoria dall'istogramma corrente
     }
 
     // Creazione del canvas e disegno dell'istogramma
@@ -91,13 +91,13 @@ int main() {
     canvas->Draw();
 
  // Creazione del canvas e disegno dell'istogramma
-    TCanvas *c = new TCanvas("canvas", "canvas", 800, 600);
+    /* TCanvas *c = new TCanvas("canvas", "canvas", 800, 600);
     sumcutHistogram->Draw("hist");
 
     // Salva l'istogramma su un file o visualizzalo a schermo
     c->SaveAs("time_distribution.png");
     c->Print("time_distribution.pdf");
-    c->Draw();
+    c->Draw();*/
 
 
 
