17a6bf34a2553363bc6ac05156766cec6efbf092
Viola DiSilvestre <disilves@neutrino-01.novalocal>
2024-02-26 17:01:40 +0100

    new commit

--- time_distribution.C
+++ time_distribution.C
@@ -8,6 +8,7 @@
 #include "TH1F.h"
 #include "TCanvas.h"
 #include "TF1.h"
+#include "TGraph.h"
 
 // Funzione per generare numeri casuali distribuiti gaussianamente
 double generateGaussian(double mean, double sigma, std::mt19937 &gen) {
@@ -15,23 +16,27 @@ double generateGaussian(double mean, double sigma, std::mt19937 &gen) {
     return distribution(gen);
 }
 
+// Funzione da plottare sopra agli istogrammi
+double myFunction(double x) {
+  double tau=3.08;
+  double a =0.588;
+  return (1/tau)*(std::pow(x/tau,(1/a)-1)/std::pow(1+std::pow(x/tau,1/a),2));
+}
+
 int main() {
-    // Apertura del file
+    // Apertura del file e lettura dei dati
     std::ifstream file("dati.txt");
     if (!file.is_open()) {
         std::cerr << "Impossibile aprire il file!" << std::endl;
         return 1;
     }
 
-    // Lettura dei dati dal file e inserimento in un vector
     std::vector<double> time;
-    std::string lineString; // Rinominata la variabile 'line' in 'lineString'
+    std::string lineString;
     while (std::getline(file, lineString)) {
         double value = std::stod(lineString);
         time.push_back(value);
     }
-
-    // Chiusura del file dopo la lettura
     file.close();
 
     // Trova il valore minimo per la traslazione
@@ -44,58 +49,58 @@ int main() {
 
     // Creazione dell'istogramma 'sumHistogram'
     TH1F* sumHistogram = new TH1F("sumHistogram", "Sum of Normalized Histograms", 400, 0, 30);
-
     sumHistogram->SetFillColor(kBlue);
     sumHistogram->SetXTitle("time [ns]");
     sumHistogram->SetYTitle("Voltage [V]");
 
-    // Seleziona i primi n eventi dal vettore time (o meno se il vettore ha meno di n elementi)
+    // Seleziona i primi n eventi dal vettore time
     int numEventsToSelect = std::min(500, static_cast<int>(time.size()));
-
-    // Creazione di gaussiane solo per gli eventi selezionati in modo casuale
     double sigma = 0.35;
 
     for (int i = 0; i < numEventsToSelect; ++i) {
-        double mean = time[i] - minTime; // Traslazione negativa per rimuovere l'offset
+        double mean = time[i] - minTime;
         TH1F* histogram = new TH1F("histogram", "Normalized Histogram", 400, 0, 30);
 
-        // Riempimento e normalizzazione dell'istogramma corrente
         for (int j = 0; j < 100000; ++j) {
             histogram->Fill(generateGaussian(mean, sigma, g));
         }
 
         histogram->Scale(1.0 / 100000);
-
-        // Aggiungi i contenuti dell'istogramma corrente a sumHistogram
         sumHistogram->Add(histogram);
-
-        delete histogram; // Liberazione della memoria dall'istogramma corrente
+        delete histogram;
     }
 
     // Creazione dell'istogramma 'realHistogram'
-    TH1F* realHistogram = new TH1F("realHistogram", "Time Distribution", 80, 58, 80);
-
+    TH1F* realHistogram = new TH1F("realHistogram", "Time Distribution", 80, 0, 30);
     realHistogram->SetFillColor(kViolet);
     realHistogram->SetXTitle("time [ns]");
     realHistogram->SetYTitle("# photoelectrons");
 
-    // Creazione di gaussiane per tutti i dati senza selezionare casualmente
-    for (size_t i = 0; i < time.size(); ++i) {
-         realHistogram->Fill(time[i]);
+    for (int i = 0; i < time.size(); ++i) {
+         realHistogram->Fill(time[i]-minTime);
     }
-    // Creazione del canvas e disegno degli istogrammi
-    TCanvas *canvasHist = new TCanvas("canvasHist", "canvasHist", 800, 600);
+    double F[time.size()];
+    double x[time.size()];
+    for (int i = 0; i < time.size(); ++i) {
+        F[i] = myFunction(time[i]-minTime);
+	x[i]=time[i]-minTime;
+    }
+
+    TGraph *graph = new TGraph(time.size(), x, F);
+
+    // Creazione dei canvas e disegno degli istogrammi
+    TCanvas *canvasSumHist = new TCanvas("canvasSumHist", "Sum of Normalized Histograms", 800, 600);
     sumHistogram->Draw("hist");
-    TCanvas *c =new TCanvas ("c","c",800,600);
+    graph->Draw("AL");
+    TCanvas *canvasRealHist = new TCanvas("canvasRealHist", "Time Distribution", 800, 600);
     realHistogram->Draw("hist");
-    
-    
-    // Salva il canvas degli istogrammi su file
-    canvasHist->SaveAs("time_distribution_all.pdf");
-    c->SaveAs("time_distribution_all_and_real.pdf");
-    canvasHist->Draw();
-    c->Draw();
-    
+    graph->Draw("AL");
+  
+
+
+    // Salva i canvas degli istogrammi su file
+    canvasSumHist->SaveAs("time_distribution_sum.pdf");
+    canvasRealHist->SaveAs("time_distribution_real.pdf");
+
     return 0;
 }
-
