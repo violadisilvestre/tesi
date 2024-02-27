751d0a933682e69b54b98c2395eaa6fedd22a329
Viola DiSilvestre <disilves@neutrino-01.novalocal>
2024-02-27 11:51:52 +0100

    new commit

--- time_distribution.C
+++ time_distribution.C
@@ -100,7 +100,7 @@ int main() {
     // Creazione dei canvas e disegno degli istogrammi
     TCanvas *canvasSumHist = new TCanvas("canvasSumHist", "Sum of Normalized Histograms", 800, 600);
     //sumHistogram->Draw("hist");
-    graph->Draw("same");
+    graph->Draw();
 
     TCanvas *canvasRealHist = new TCanvas("canvasRealHist", "Time Distribution", 800, 600);
     realHistogram->Draw("hist");
