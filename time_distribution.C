04753a34424444c415e2e24700c544683e5ad558
Viola DiSilvestre <disilves@neutrino-01.novalocal>
2024-02-28 15:13:43 +0100

    new commit

--- time_distribution.C
+++ time_distribution.C
@@ -28,7 +28,7 @@ double myFunction(double x,int dim) {
 
 int main() {
     // Apertura del file e lettura dei dati
-    std::ifstream file("dati.txt");
+    std::ifstream file("dati_100.txt");
     if (!file.is_open()) {
         std::cerr << "Impossibile aprire il file!" << std::endl;
         return 1;
@@ -126,7 +126,7 @@ int main() {
     // graph->Draw("same"); 
   
     // Salva i canvas degli istogrammi su file
-    canvasSumHist->SaveAs("time_distribution_sum_1000.pdf");
+    canvasSumHist->SaveAs("time_distribution_sum_100.pdf");
     //canvasRealHist->SaveAs("time_distribution_real.pdf");
 
     return 0;
