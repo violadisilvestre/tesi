409521223e017dc002f4493dfb442ef9eae775bc
Viola DiSilvestre <disilves@neutrino-01.novalocal>
2024-02-29 10:28:04 +0100

    new commit

--- time_distribution.C
+++ time_distribution.C
@@ -90,7 +90,7 @@ int main() {
     // Normalization factor for sumHistogram
     double normalizationFactorSum = 1.0 / sumHistogram->Integral();
     sumHistogram->Scale(normalizationFactorSum);
-    int num=12;
+    int num=11;
     double F[num];
     double x[num];
     double x_gaus[num];
@@ -116,7 +116,7 @@ int main() {
     // Creazione dei canvas e disegno degli istogrammi
     TCanvas *canvasSumHist = new TCanvas("canvasSumHist", "Photoelectron time distribution", 800, 600);
     sumHistogram->Draw("hist");
-    sumHistogram->GetYaxis()->SetRangeUser(0, 0.35);
+    sumHistogram->GetYaxis()->SetRangeUser(0, 0.4);
     graph->Draw("same");
    
     
