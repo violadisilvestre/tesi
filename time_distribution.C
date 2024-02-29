c53ea73d9d7f650c6aa1e80ad11c8f6247ef43ff
Viola DiSilvestre <disilves@neutrino-01.novalocal>
2024-02-29 11:00:28 +0100

    new commit

--- time_distribution.C
+++ time_distribution.C
@@ -31,7 +31,7 @@ int main() {
 
    const int bin=25;
    const int x_max=25;
-   const int num=1500;
+   const int num=2000;
    Color_t myColor = kGreen;
 
     // Apertura del file e lettura dei dati
