873e9fdab1f3e4c2620e506cce1b8c55757b08fc
Viola DiSilvestre <disilves@neutrino-01.novalocal>
2024-03-01 10:56:50 +0100

    new commit

--- time_distribution.C
+++ time_distribution.C
@@ -65,7 +65,7 @@ int main() {
     sumHistogram->SetXTitle("time [ns]");
     sumHistogram->SetYTitle("Counts"); // Update the y-axis title
 
-    double sigma = 0.035;
+    double sigma = 0.35;
     std::sort(time.begin(), time.end());
     for (int i = 0; i < time.size(); ++i) {
         double mean = time[i];
