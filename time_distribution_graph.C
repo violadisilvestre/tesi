a3d77a610e6409032e4859386390f6ac22ff685e
Viola DiSilvestre <disilves@neutrino-01.novalocal>
2024-03-09 14:43:38 +0100

    new commit

--- time_distribution_graph.C
+++ time_distribution_graph.C
@@ -112,7 +112,7 @@ int main() {
     // Creazione dei canvas e disegno degli istogrammi
     TCanvas *canvasSumHist = new TCanvas("canvasSumHist", "Photoelectron time distribution", 800, 600);
     gaussian->Draw();
-    gaussian->GetYaxis()->SetRangeUser(0,45);
+    gaussian->GetYaxis()->SetRangeUser(0,50);
     gaussian->GetXaxis()->SetRangeUser(0, x_max);
     graph->Draw("same");
    
