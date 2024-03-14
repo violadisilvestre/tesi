ab762512dea9f13ac3debcef01e4b379faa8eeae
Viola DiSilvestre <disilves@neutrino-01.novalocal>
2024-03-14 13:16:43 +0100

    new commit

--- time_distribution_graph.C
+++ time_distribution_graph.C
@@ -76,7 +76,7 @@ int main() {
     double y[num];
     std::default_random_engine generator;
     std::uniform_real_distribution<double> distribution(-1,25);
-    std::uniform_real_distribution<double> d(5,25);
+    std::uniform_real_distribution<double> d(0,25);
     
     for (int i = 0; i < num; ++i) {
       
