01d5a9b2302277c4c89f399fce1b45920090de80
Viola DiSilvestre <disilves@neutrino-01.novalocal>
2024-03-19 16:52:56 +0100

    new commit

--- time_distribution_graph.C
+++ time_distribution_graph.C
@@ -129,7 +129,7 @@ int main() {
 
    
     // Salva i canvas degli istogrammi su file
-    canvasSumHist->SaveAs("time_distribution_evt4_mu.pdf");
+    canvasSumHist->SaveAs("time_distribution_evt0_mu.pdf");
     
 
     return 0;
