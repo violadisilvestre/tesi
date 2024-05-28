cba1f2c0b9b55e8f91b1fee829bc3f7a97ec5ec5
Viola DiSilvestre <disilves@neutrino-01.novalocal>
2024-05-28 14:46:28 +0200

    new commit

--- time_distribution_graph.C
+++ time_distribution_graph.C
@@ -84,8 +84,8 @@ int main() {
     double F[num];
     double G[num];
     std::default_random_engine generator;
-    std::uniform_real_distribution<double> distribution(-1, 25);
-    std::uniform_real_distribution<double> d(0, 25);
+    std::uniform_real_distribution<double> distribution(-1, 20);
+    std::uniform_real_distribution<double> d(0, 20);
     
     for (int i = 0; i < num; ++i) {
         x[i] = distribution(generator);
@@ -142,8 +142,8 @@ int main() {
     gaussian->GetYaxis()->SetTitleSize(0.04);
 
     // Set the x-axis range for both graphs
-    gaussian->GetXaxis()->SetLimits(-1, 35);
-    graph->GetXaxis()->SetLimits(-1, 35);
+    //gaussian->GetXaxis()->SetLimits(-1, 35);
+    //graph->GetXaxis()->SetLimits(-1, 35);
 
     // Creazione dei canvas e disegno degli istogrammi
     TCanvas* canvasSumHist = new TCanvas("canvasSumHist", "Photoelectron time distribution", 800, 600);
