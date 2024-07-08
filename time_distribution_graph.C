833fb631a550f467027adada90a762ac8055b7fd
Viola DiSilvestre <disilves@neutrino-01.novalocal>
2024-07-08 11:52:03 +0200

    new commit

--- time_distribution_graph.C
+++ time_distribution_graph.C
@@ -221,7 +221,7 @@ int main() {
         "T_smear_0_800.txt", "T_smear_24044_1000.txt",
         "T_smear_26539_2000.txt",
         "T_smear_41806_4000.txt", "T_smear_44519_2000.txt",
-        "T_smear_5275_2800.txt", "T_smear_57_63.txt",
+        "T_smear_5275_2800.txt",
         "T_smear_67_150.txt", "T_smear_86_22.txt",
         "T_smear_4000.txt","T_smear_119.txt","T_smear_325.txt","T_smear_723.txt","T_smear_1122.txt","T_smear_29824.txt","T_smear_12392.txt"
     };
@@ -245,7 +245,7 @@ int main() {
     gr_low->SetMarkerStyle(20); // Imposta lo stile dei punti
     gr_low->SetMarkerColor(kBlue);
     gr_low->GetYaxis()->SetRangeUser(0,20);
-    gr_low->GetXaxis()->SetRangeUser(0, 1800);
+    gr_low->GetXaxis()->SetRangeUser(0, 1470);
     gr_low->Draw("AP");
 
     // Disegna il secondo grafico sullo stesso canvas
@@ -260,8 +260,8 @@ int main() {
     legend->Draw();
 
     // Esegui il fitting dei dati
-    TF1 *fit_low = new TF1("fit_low", "pol3", 0.01, 1800); // Fitting con un polinomio di secondo grado
-    TF1 *fit_high = new TF1("fit_high", "pol3", 0.01, 1800);
+    TF1 *fit_low = new TF1("fit_low", "pol5", -0.01, 1470); // Fitting con un polinomio di secondo grado
+    TF1 *fit_high = new TF1("fit_high", "pol5", -0.01, 1470);
 
     gr_low->Fit(fit_low, "R");
     gr_high->Fit(fit_high, "R");
