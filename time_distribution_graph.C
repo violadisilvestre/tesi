e4b2509fa52059c1ad1802109928150a1ccbd0c5
Viola DiSilvestre <disilves@neutrino-01.novalocal>
2024-07-10 12:06:01 +0200

    new commit

--- time_distribution_graph.C
+++ time_distribution_graph.C
@@ -296,8 +296,8 @@ int main() {
     legend->Draw();
 
     // Esegui il fitting dei dati
-    TF1 *fit_low = new TF1("fit_low", "[0]+[1]*x+[2]*x**2+[3]*x**4", 0, 20); // Fitting con un polinomio di secondo grado
-    TF1 *fit_high = new TF1("fit_high", "[0]+[1]*x+[2]*x**2+[3]*x**4", 0, 20);
+    TF1 *fit_low = new TF1("fit_low", "[0]+[1]*x+[2]*x**2+[3]*x**3", 0, 20); // Fitting con un polinomio di secondo grado
+    TF1 *fit_high = new TF1("fit_high", "[0]+[1]*x+[2]*x**2+[3]*x**3", 0, 20);
 
     gr_low->Fit(fit_low);
     gr_high->Fit(fit_high);
@@ -312,7 +312,7 @@ int main() {
     fit_high->Draw("same");
 
     // Salva il grafico in un file
-    c1->SaveAs("N_vs_ToT_fit4.png");
+    c1->SaveAs("N_vs_ToT_fit5.png");
 
     // Pulizia della memoria
     delete gr_low;
