c4fcfd051340116cdb14e1f998f5e943bf5d484b
Viola DiSilvestre <disilves@neutrino-01.novalocal>
2024-07-10 10:35:54 +0200

    new commit

--- time_distribution_graph.C
+++ time_distribution_graph.C
@@ -217,12 +217,12 @@ void processFile(const std::string& filename, std::vector<double>& N, std::vecto
 }
 int main() {
     std::vector<std::string> filenames = {
-        "T_smear_0_800.txt", "T_smear_24044_1000.txt",
+        "T_smear_0_800.txt",
         "T_smear_26539_2000.txt",
         "T_smear_41806_4000.txt", "T_smear_44519_2000.txt",
         "T_smear_5275_2800.txt",
         "T_smear_67_150.txt", "T_smear_86_22.txt",
-        "T_smear_4000.txt","T_smear_119.txt","T_smear_325.txt","T_smear_723.txt","T_smear_29824.txt","T_smear_12392.txt","T_smear_325.txt","T_smear_528.txt","T_smear_648.txt","T_smear_26539.txt","T_smear_71004.txt","T_smear_89031.txt"
+        "T_smear_4000.txt","T_smear_119.txt","T_smear_325.txt","T_smear_723.txt","T_smear_29824.txt","T_smear_12392.txt","T_smear_325.txt","T_smear_528.txt","T_smear_648.txt","T_smear_26539.txt","T_smear_89031.txt"
     };
 
     std::vector<double> N;
