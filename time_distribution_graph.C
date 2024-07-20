b34376b9110fcbd84dc3730d9f49e4f88647e5b6
Viola DiSilvestre <disilves@neutrino-01.novalocal>
2024-07-20 13:45:30 +0200

    new commit

--- time_distribution_graph.C
+++ time_distribution_graph.C
@@ -95,7 +95,7 @@ void processFile(const std::string& filename, std::vector<double>& N, std::vecto
     // Calcolo delle funzioni G e F
     std::vector<double> G(NUM_POINTS), F(NUM_POINTS);
     for (int i = 0; i < NUM_POINTS; ++i) {
-      G[i] = Gaussian_sum(x[i], times,0.6);
+      G[i] = Gaussian_sum(x[i], times,0.4);
       F[i] = myFunction(y[i], times.size());
     }
 
@@ -204,7 +204,7 @@ void processFile(const std::string& filename, std::vector<double>& N, std::vecto
       //legend->Draw();
       
       // Salvataggio del canvas su file
-      std::string outputFilename = "TTS_0.8_exp0.6" + filename + ".png";
+      std::string outputFilename = "TTS_0.8_exp0.4" + filename + ".png";
       canvas->SaveAs(outputFilename.c_str());
       
       // Pulizia della memoria
