66fb6a5fae0fd3159dee4c8cf0ba213ca64b28e7
Viola DiSilvestre <disilves@neutrino-01.novalocal>
2024-07-20 13:41:35 +0200

    new commit

--- time_distribution_graph.C
+++ time_distribution_graph.C
@@ -26,11 +26,11 @@ double gaussian(double x, double mean, double stddev) {
 }
 
 // Somma di gaussiane
-double Gaussian_sum(double x, const std::vector<double>& times, double tpe,double tau) {
+double Gaussian_sum(double x, const std::vector<double>& times, double tpe) {
     double G = 0;
     for (double t : times) {
         double gaussian_value = gaussian(x, t, GAUSSIAN_STDDEV);
-        double exponential_part = exp(-(x-t)/tau);
+        double exponential_part = exp(-(x*tpe));
         G += gaussian_value * exponential_part;
     }
     return G;
@@ -95,7 +95,7 @@ void processFile(const std::string& filename, std::vector<double>& N, std::vecto
     // Calcolo delle funzioni G e F
     std::vector<double> G(NUM_POINTS), F(NUM_POINTS);
     for (int i = 0; i < NUM_POINTS; ++i) {
-      G[i] = Gaussian_sum(x[i], times,0.1,0.3);
+      G[i] = Gaussian_sum(x[i], times,0.6);
       F[i] = myFunction(y[i], times.size());
     }
 
@@ -204,7 +204,7 @@ void processFile(const std::string& filename, std::vector<double>& N, std::vecto
       //legend->Draw();
       
       // Salvataggio del canvas su file
-      std::string outputFilename = "TTS_0.8_exp0.1_tau_0.3_" + filename + ".png";
+      std::string outputFilename = "TTS_0.8_exp0.6" + filename + ".png";
       canvas->SaveAs(outputFilename.c_str());
       
       // Pulizia della memoria
